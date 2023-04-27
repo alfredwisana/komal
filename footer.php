d80bfd54ace80b20e20594f6b318282d86bc6249
jbgaming43 <jbhanjaya@gmail.com>
2023-04-27 22:50:45 +0700

    edit footer

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         
         <div id="copyright" font-family: roboto;
         text-align: center;
@@ -12,9 +13,6 @@
                 <div class="container bottom_border">
                     <div class="row">
                         <div class=" col-sm-4 col-md col-sm-4  col-12 col">
-                            <h5 class="headin5_amrc col_white_amrc pt2">About us</h5>
-                            <!--headin5_amrc-->
-                            <p class="fa font-awesome">We are the best and most complete baby shop in this town with the best price and high quality.</p>
                             <p><i class="fa fa-location-arrow"></i> Jl. Magersari, RT.1/RW.1, Magersari, Krian, Sidoarjo, Kabupaten Sidoarjo, Jawa Timur </p>
                             <p><i class="fa fa-phone"></i> 085748177559 </p>
                             <p><i class="fa fa-clock"></i> 7AM-9:30PM </p>
@@ -37,14 +35,15 @@
                             <!--headin5_amrc ends here-->
 
                             <ul class="footer_ul2_amrc">
-                                <li><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-facebook"></span></a> </a>
-                                    <a href="https://Facebook.com" title="Facebook" alt="Facebook">Facebook</a>
+                                <li><i class="fa-solid fa-bag-shopping" style="color: #4ce821;"></i>
+                                    <a href="https://web.Tokopedia.com/" title="Tokped" alt="Tokped" style="text-decoration: none; color:black">Facebook</a>
                                 </li>
-                                <li><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-instagram"></span></a> </a>
-                                    <a href="https://instagram.com" title="Instagram" alt="Instagram">Instagram</a>
+                                <li><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-instagram" style="color:black"></span></a> </a>
+                                    <a href="https://instagram.com" title="Instagram" alt="Instagram" style="text-decoration: none; color:black">Instagram</a>
                                 </li>
-                                <li><a href="#"><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-youtube"></span></a> </a>
-                                    <a href="https://youtube.com" title="Youtube" alt="Youtube">Youtube</a>
+                                <li><i class="fa-solid fa-bag-shopping" style="color: #ff6600;"></i>
+                                    <a href="https://youtube.com" title="Shopee" alt="Shopee" style="text-decoration: none; color:black">Shopee</a>
+                                    
                                 </li>
                             </ul>
                             <!--footer_ul2_amrc ends here-->

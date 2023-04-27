26f1d3303ba5c7a8592d09abb93ca4fd1cd0bea2
Alfred Wisana <117831014+alfredwisana@users.noreply.github.com>
2023-04-27 21:38:08 +0700

    login + footer

---
+++ footer.php
@@ -0,0 +1,68 @@
+<!-- Footer/Copyright -->
+        <!--footer starts from here-->
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+        
+        <div id="copyright" font-family: roboto;
+        text-align: center;
+        color: #fff;>>
+            <footer class="footer">
+                <div class="container bottom_border">
+                    <div class="row">
+                        <div class=" col-sm-4 col-md col-sm-4  col-12 col">
+                            <h5 class="headin5_amrc col_white_amrc pt2">About us</h5>
+                            <!--headin5_amrc-->
+                            <p class="fa font-awesome">We are the best and most complete baby shop in this town with the best price and high quality.</p>
+                            <p><i class="fa fa-location-arrow"></i> Jl. Magersari, RT.1/RW.1, Magersari, Krian, Sidoarjo, Kabupaten Sidoarjo, Jawa Timur </p>
+                            <p><i class="fa fa-phone"></i> 085748177559 </p>
+                            <p><i class="fa fa-clock"></i> 7AM-9:30PM </p>
+                        </div>
+                        <div class=" col-sm-4 col-md  col-6 col">
+                            <h5 class="headin5_amrc col_white_amrc pt2">Contact Us</h5>
+                            <!--headin5_amrc-->
+                            <p class="mb10">Senin - Minggu</p>
+                            <a href="https://web.Tokopedia.com/" class="btn" style="background-color: lightgreen
+                            ;"> Tokopedia</a>
+                            <br>
+                            <br>
+                            <a href="https://web.whatsapp.com/" class="btn" style="background-color: lightgreen
+                            ;"><i class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-whatsapp"></span></i> Hubungi di Whatsapp</a>
+                            <!--footer_ul_amrc ends here-->
+                        </div>
+
+                        <div class=" col-sm-4 col-md  col-12 col">
+                            <h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+                            <!--headin5_amrc ends here-->
+
+                            <ul class="footer_ul2_amrc">
+                                <li><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-facebook"></span></a> </a>
+                                    <a href="https://Facebook.com" title="Facebook" alt="Facebook">Facebook</a>
+                                </li>
+                                <li><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-instagram"></span></a> </a>
+                                    <a href="https://instagram.com" title="Instagram" alt="Instagram">Instagram</a>
+                                </li>
+                                <li><a href="#"><a class="social-media-icon" href="https://link_social_mendia_anda"><span class="fab fa-youtube"></span></a> </a>
+                                    <a href="https://youtube.com" title="Youtube" alt="Youtube">Youtube</a>
+                                </li>
+                            </ul>
+                            <!--footer_ul2_amrc ends here-->
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container">
+                    <!--foote_bottom_ul_amrc ends here-->
+                    <p class="text-center">Copyright @2023 | Designed by Learning Service Team Universitas Kristen Petra</p>
+                    <!--social_footer_ul ends here-->
+                    
+                </div>
+            </footer>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        </div>
+
+</html>
---
+++ login.php
@@ -0,0 +1,124 @@
+<?php
+require 'connect.php';
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Website Pijat</title>
+
+    <!-- bootstrap 5.2 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <style>
+        body {
+            /* background-position: center; */
+            background-repeat: no-repeat;
+            background-size: cover; 
+            overflow-x: hidden;
+        }
+
+        form {
+            margin-top: 130px;
+            margin-left: 450px;
+            margin-right: 450px;
+            padding-left: 100px;
+            padding-right: 100px;
+            padding-top: 50px;
+            padding-bottom: 50px;
+            background-color: rgb(200, 211, 184);
+            border-radius: 20px;
+        }
+        button {
+          --color: #9AAF7D;
+          font-family: inherit;
+          display: inline-block;
+          width: 6em;
+          height: 2.6em;
+          line-height: 2.5em;
+          overflow: hidden;
+          margin: 20px;
+          font-size: 17px;
+          z-index: 1;
+          color: var(--color);
+          border: 2px solid var(--color);
+          border-radius: 15px;
+          position: relative;
+        }
+
+        button::before {
+          position: absolute;
+          content: "";
+          background: var(--color);
+          width: 150px;
+          height: 200px;
+          z-index: -1;
+          border-radius: 50%;
+        }
+
+        button:hover {
+          color: white;
+        }
+
+        button:before {
+          top: 100%;
+          left: 100%;
+          transition: .3s all;
+        }
+
+        button:hover::before {
+          top: -30px;
+          left: -30px;
+        }
+        .input input {
+          border-radius: 10px;
+          outline: 2px solid #9AAF7D;
+          border: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+          background-color: #edf0e7;
+          outline-offset: 3px;
+          padding: 10px 1rem;
+          transition: 0.25s;
+        }
+
+        .input input:focus {
+          outline-offset: 5px;
+          background-color: #edf0e7;
+        }
+        .input{
+            padding-top: 10px;
+        }
+    </style>
+</head>
+
+<body background="https://mediaini.com/wp-content/uploads/2021/01/Rekomendasi-perlengkapan-bayi-Instagram.jpg">
+    <div class="wrapper">
+        <form action="loginprocess.php" method="post">
+            <h2 style=" padding-bottom: 15px;">Login</h2>
+            <div class="input">
+                <input type="text" class="form-control" id="exampleInputEmail1" name="username" placeholder="Username">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="input" style="padding-top:30px;">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+            </div>
+            <div class="text-center">
+                <button type="submit" name="login">Login</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>

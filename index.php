bdf78d08241338df0d0bb2e85f861492eae1a47b
oCharybd1s <108251057+oCharybd1s@users.noreply.github.com>
2023-04-29 00:31:28 +0700

    Kategori Index.php

--- index.php
+++ index.php
@@ -48,6 +48,8 @@ $res = mysqli_query($con, $remind);
     <!-- JS -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css" integrity="sha512-xxxxxx" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.bundle.min.js" integrity="sha512-xxxxxx" crossorigin="anonymous"></script>
 
     <style>
         /* custom scrollbar */
@@ -70,21 +72,37 @@ $res = mysqli_query($con, $remind);
 
         /* Handle on hover */
         ::-webkit-scrollbar-thumb:hover {
-            background: lightblue;
-        }
-
-        .btn:hover {
-            transform: scale(1.2);
-            transition: .2s;
+            background: #beafe1;
         }
 
         .card {
+        width: 300px;
+        height: 400px;
+        background-color: #eee7d3;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+        overflow: hidden;
         margin-bottom: 30px;
         }
 
         .card-img-top {
         height: 200px;
         object-fit: cover;
+        }
+
+        .card-body {
+        padding: 20px;
+        }
+
+        .card-text {
+        font-size: 16px;
+        margin-bottom: 20px;
+        }
+
+        .card-title {
+        font-size: 24px;
+        font-weight: bold;
+        margin-bottom: 10px;
         }
 
         .card-price {
@@ -98,26 +116,111 @@ $res = mysqli_query($con, $remind);
             height: 150px;
         }
         }
+
+        .btn {
+        display: inline-block;
+        padding: 10px 20px;
+        background-color: #C8d3b8;
+        color: white;
+        text-decoration: none;
+        border-radius: 5px;
+        font-size: 16px;
+        font-weight: bold;
+        transition: background-color 0.3s ease;
+        }
+
+        .btn:hover {
+        background-color: #beafe1;
+        }
+         */
+
+        nav {
+        font-family: Arial, sans-serif;
+        }
+
+        nav a {
+        color: #333;
+        text-decoration: none;
+        padding: 10px;
+        }
+
+        nav .nav-link.active {
+        font-weight: bold;
+        }
+
+        nav ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        }
+
+        nav ul li {
+        display: inline-block;
+        padding: 10px;
+        }
+
+        nav ul li a {
+        display: block;
+        padding: 10px;
+        text-decoration: none;
+        }
+
+        nav ul li:hover > .dropdown-content {
+        display: block;
+        }
+
+        .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #d9b4e2;
+        z-index: 1;
+        }
+
+        .dropdown-content li {
+        display: block;
+        
+        }
+
+        .dropdown-content li a {
+        color: #333;
+        padding: 5px;
+        text-decoration: none;
+        }
+
+        .dropdown-content li:hover {
+        margin-top: 8px;
+        display: block;
+        padding: 10px;
+        background-color: #beafe1;
+        }
+
     </style>
 </head>
 
-<body>
+<body style="background-color:#EAD7c3">
     <!-- navabar -->
-    <nav class="navbar navbar-expand-lg bg-light mb-5">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Website Pijat</a>
+    <nav class="navbar navbar-expand-lg mb-5" style="background-color:#d9b4e2">
+        <div class="container-fluid" >
+            <h1 class="navbar-brand" href="index.php">Website Pijat</h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-center mb-lg-0">
-                    <li class="nav-item" style="margin-right:50px">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                <ul class="navbar-nav me-auto">
+                    <li class="Home">
+                        <a href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <li class="dropdown">
+                        <a href="#">Kategori</a>
+                        <ul class="dropdown-content">
+                            <li><a href="#">Atasan</a></li>
+                            <li><a href="#">Bawahan</a></li>
+                            <li><a href="#">Dress</a></li>
+                            <li><a href="#">Aksesoris</a></li>
+                            <li><a href="#">Lain-Lain</a></li>
+                        </ul>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
@@ -126,10 +229,9 @@ $res = mysqli_query($con, $remind);
                 </form>
                 <ul class="navbar-nav mb-5 d-flex mb-lg-0" id="loginlink">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="logout.php">Logout</a>
+                        <a class="btn btn-primary" aria-current="page" href="login/logout.php">Logout</a>
                     </li>
                 </ul>
-
             </div>
         </div>
     </nav>
@@ -158,7 +260,6 @@ $res = mysqli_query($con, $remind);
                 $sql = "SELECT * FROM services";
                 $result = mysqli_query($con, $query);
 
-                // Looping data barang
                 while($row = mysqli_fetch_assoc($result)){
                 ?>
                 <div class="col-md-3">
@@ -168,7 +269,7 @@ $res = mysqli_query($con, $remind);
                             <h5 class="card-title"><?php echo $row['namaServis'] ?></h5>
                             <p class="card-text"><?php echo $row['deskripsi'] ?></p>
                             <p class="card-price">Rp <?php echo $row['harga'] ?></p>
-                            <a href="#" class="btn btn-primary">Booking</a>
+                            <a href="booking.php" class="btn btn-primary">Booking</a>
                         </div>
                     </div>
                 </div>

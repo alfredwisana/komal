e41e7f00617cd97795faa446a7bbd30742e8c37d
Alfred Wisana <117831014+alfredwisana@users.noreply.github.com>
2023-05-01 01:13:51 +0700

    perbaiki indetasi

--- admin/delCat.php
+++ admin/delCat.php
@@ -14,9 +14,9 @@ if (isset($_POST['Kategori'])) {
     $Kategori = $_POST['Kategori'];
 
 
-$sql = "DELETE FROM `category` WHERE namaKategori = '$Kategori'";
-$query = mysqli_query($con, $sql);
-$result['message'] = "Barang berhasil dihapus";
-echo json_encode($result);
-}
+    $sql = "DELETE FROM `category` WHERE namaKategori = '$Kategori'";
+    $query = mysqli_query($con, $sql);
+    $result['message'] = "Barang berhasil dihapus";
+    echo json_encode($result);
+    }
 ?>

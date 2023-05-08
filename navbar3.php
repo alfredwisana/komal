e583ea57b1166637a7b1d698086b34a3f0869700
KristianACW <86514218+KristianACW@users.noreply.github.com>
2023-05-08 14:04:26 +0700

    Update navbar3.php

--- navbar3.php
+++ navbar3.php
@@ -2,27 +2,25 @@
 <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-
-<!-- navabar -->
-<nav class="navbar navbar-expand-lg bg-light" id="navbar">
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script><!-- navabar -->
+    <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <img src="images/logo.png" style="width: 3rem; margin-right: 0.5rem;">
+            <img src="../images/logo.png" style="width: 3rem; margin-right: 1.1rem;">
             <h5 style="margin-top: 0.3rem;">Berkah Jaya Baby Shop </h5>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent"style="width: 3rem; margin-left: 0.5rem;">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent"style="width: 3rem;">
+                <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-center mb-lg-0">
+                </ul>
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="home.php" class="nav-link text-uppercase font-weight-bold">Home <span class="sr-only">(current)</span></a></li>
+                    <li class="nav-item active"><a href="home.php" class="nav-link text-uppercase font-weight-bold">Home</a></li>
                             <li class="nav-item"><a href="about.php" class="nav-link text-uppercase font-weight-bold">About us</a></li>
                             <li class="nav-item"><a href="catalog.php" class="nav-link text-uppercase font-weight-bold">Catalog</a></li>
                             <li class="nav-item"><a href="login/index.php" class="nav-link text-uppercase font-weight-bold">Login</a></li>
                     </ul>
-                <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-center mb-lg-0">
-                </ul>
             </div>
         </div>
     </nav>

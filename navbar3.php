748791ba8aacc8ec85eb0f1c49ee53927e52582b
zereftyzzz <c14210184@john.petra.ac.id>
2023-05-08 13:49:43 +0700

    edit navbar dikit

--- navbar3.php
+++ navbar3.php
@@ -1,10 +1,13 @@
 <!-- JQuery -->
 <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
 <!-- navabar -->
 <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <img src="../images/logo.png" style="width: 3rem; margin-right: 0.5rem;">
+            <img src="images/logo.png" style="width: 3rem; margin-right: 0.5rem;">
             <h5 style="margin-top: 0.3rem;">Berkah Jaya Baby Shop </h5>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"

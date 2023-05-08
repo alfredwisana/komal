04069f4cf88502b6b7a243de91d5fcba009299b2
KristianACW <86514218+KristianACW@users.noreply.github.com>
2023-05-08 14:19:17 +0700

    Update navbar3.php

--- navbar3.php
+++ navbar3.php
@@ -2,7 +2,8 @@
 <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script><!-- navabar -->
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<!-- navabar -->
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <img src="../images/logo.png" style="width: 3rem; margin-right: 1.1rem;">
@@ -16,8 +17,8 @@
                 <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-center mb-lg-0">
                 </ul>
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="home.php" class="nav-link text-uppercase font-weight-bold">Home</a></li>
-                            <li class="nav-item"><a href="about.php" class="nav-link text-uppercase font-weight-bold">About us</a></li>
+                    <li class="nav-item active"><a href="rumah.php" class="nav-link text-uppercase font-weight-bold">Home</a></li>
+                            <li class="nav-item"><a href="about_us.php" class="nav-link text-uppercase font-weight-bold">About us</a></li>
                             <li class="nav-item"><a href="catalog.php" class="nav-link text-uppercase font-weight-bold">Catalog</a></li>
                             <li class="nav-item"><a href="login/index.php" class="nav-link text-uppercase font-weight-bold">Login</a></li>
                     </ul>

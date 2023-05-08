25d64f90c122a9ae975f3609dc651fc22c12ae85
Maiikuu\elmac <elmacopicolo9@gmail.com>
2023-05-08 14:21:10 +0700

    ganti href home & about

--- navbar3.php
+++ navbar3.php
@@ -5,7 +5,7 @@
 <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script><!-- navabar -->
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <img src="../images/logo.png" style="width: 3rem; margin-right: 1.1rem;">
+            <img src="images/logo.png" style="width: 3rem; margin-right: 1.1rem;">
             <h5 style="margin-top: 0.3rem;">Berkah Jaya Baby Shop </h5>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -16,8 +16,8 @@
                 <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-center mb-lg-0">
                 </ul>
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="home.php" class="nav-link text-uppercase font-weight-bold">Home</a></li>
-                            <li class="nav-item"><a href="about.php" class="nav-link text-uppercase font-weight-bold">About us</a></li>
+                    <li class="nav-item active"><a href="index.php" class="nav-link text-uppercase font-weight-bold">Home</a></li>
+                            <li class="nav-item"><a href="About_us.php" class="nav-link text-uppercase font-weight-bold">About us</a></li>
                             <li class="nav-item"><a href="catalog.php" class="nav-link text-uppercase font-weight-bold">Catalog</a></li>
                             <li class="nav-item"><a href="login/index.php" class="nav-link text-uppercase font-weight-bold">Login</a></li>
                     </ul>

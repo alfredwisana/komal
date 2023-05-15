87e34b977f67feb0ab250d96065bcdd1d437dccc
Frederika <c14210185@john.petra.ac.id>
2023-05-15 13:14:02 +0700

    side navbar + navbar + footer + path

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,27 +1,45 @@
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
+<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
 
-<nav class="navbar navbar-expand-lg mb-2" style="background-color: #d9b4e2;">
-            <div class="container-fluid" style="background-color: #d9b4e2;">
-                <a class="navbar-brand" href="index.php"><img src="../images/logo.png" style="height:1.5rem; width:1.5rem; margin-top: -0.2rem;"></a>
-                <button class="navbar-toggler" id="search" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-left mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav mb-5 d-flex mb-lg-0" id="addLink">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="add.php">Add</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav mb-5 d-flex mb-lg-0" id="logoutlink">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="../login/logout.php">Logout</a>
-                        </li>
-                    </ul>
+<style>
+    nav h5{
+        font: normal 30px 'Cookie', cursive;
+    }
+    nav #info{
+        font-size: 18px;
+        font-family: cursive;
+        font-weight: bold;
+        margin-left: -200px;
+    }
+    @media only screen and (max-width : 1024px) { 
+        #info{
+            margin-left: 0;
+        }
+    }
+</style>
+
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg bg-light" style="max-height: 4rem;" id="navbar4">
+        <div class="container-fluid">
+            <img src="../images/logo.png" style="width: 3rem; margin-right: 1.1rem;">
+            <h5 style="margin-top: 0.3rem;">Berkah Jaya Baby Shop </h5>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-5 col d-flex justify-content-center mb-lg-0" id="info">
+                    <li class="nav-item active"><a href="index.php" class="nav-link text-uppercase font-weight-bold">Home</a></li>
+                    <li class="nav-item"><a href="add.php" class="nav-link text-uppercase font-weight-bold">Add Item</a></li>
+                </ul>
+                <div class="navbar-icons">
+                    <a href="../login/index.php"><i class="fa-solid fa-circle-user fa-2xl" style="color: black;"></i></a>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>

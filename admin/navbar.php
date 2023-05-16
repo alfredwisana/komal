39cc57591b82d3f819143159deeb139e9e328c99
Alfred Wisana <117831014+alfredwisana@users.noreply.github.com>
2023-05-17 00:00:41 +0700

    navbar

--- admin/navbar.php
+++ admin/navbar.php
@@ -41,7 +41,7 @@
                             <li class="nav-item"><a href="./add.php" class="nav-link text-uppercase font-weight-bold">Add Item</a></li>
                 </ul>
                 <div class="navbar-icons">
-                    <a href="#"><i class="fa-solid fa-circle-user fa-2xl" style="color: black;"></i></a>
+                    <a href="#" id="logout"><i class="fa-solid fa-circle-user fa-2xl" style="color: black;"></i></a>
                 </div>
             </div>
         </div>
